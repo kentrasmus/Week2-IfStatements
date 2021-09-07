9dca3ef5f92267a7ff91be6ee0874d56b19e2e0c
Opilane TTHK <opilane@tthk.ee>
2021-09-07 12:08:28 +0300

    tolower

--- PersonalityTest/Program.cs
+++ PersonalityTest/Program.cs
@@ -13,7 +13,7 @@ namespace PersonalityTest
             //kui kasutaja sisestab "midagi muud", konsool kuvab "oled ükssarv";
 
             Console.WriteLine("Mis on su lemmikvärv");
-            string UserColor = Console.ReadLine();
+            string UserColor = Console.ReadLine().ToLower();
 
             if (UserColor == "punane")
             {
